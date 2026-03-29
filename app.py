f722e53edb96e8912ce9f569caf89211bbd483eb
valtsu41 <42544217+valtsu41@users.noreply.github.com>
2026-03-29 23:42:33 +0300

    Implement basic logins

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from datetime import datetime
+import sqlite3
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, session, request, render_template, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import db
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
@@ -11,9 +16,53 @@ def index():
     visit_count = db.query("SELECT COUNT(*) AS c FROM Visits")[0]["c"]
     return render_template("index.html", visit_count=visit_count)
 
-@app.route("/new")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/do-register", methods=["POST"])
+def do_register():
+    name = request.form["username"]
+    passwd1 = request.form["password1"]
+    passwd2 = request.form["password2"]
+    if passwd1 != passwd2:
+        return "Salasanat eivät täsmää"
+    passwd_hash = generate_password_hash(passwd1)
+    try:
+        db.execute("INSERT INTO Users (username, password_hash) VALUES (?, ?)", [name, passwd_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/do-login", methods=["POST"])
+def do_login():
+    name = request.form["username"]
+    passwd = request.form["password"]
+
+    passwd_hash = db.query("SELECT password_hash FROM Users WHERE username = ?", [name])[0][0]
+    if check_password_hash(passwd_hash, passwd):
+        session["username"] = name
+        return redirect("/")
+    else:
+        return "Virheellinen käyttäjätunnus tai salasana"
+
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/new-post")
 def new_post():
-    return render_template("new.html")
+    if "username" not in session:
+        return redirect("/login")
+    return render_template("new-post.html")
 
 @app.route("/posts")
 def posts():
@@ -21,10 +70,9 @@ def posts():
     count = len(posts)
     return render_template("posts.html", count=count, posts=posts)
 
-@app.route("/posts", methods=("POST",))
+@app.route("/posts", methods=["POST"])
 def add_post():
-    username = request.form["username"]
     item = request.form["item"]
-    db.execute("INSERT INTO Posts (author, item) VALUES (?, ?)", (username, item))
+    db.execute("INSERT INTO Posts (author, item) VALUES (?, ?)", (session["username"], item))
     return redirect("/posts")
 
