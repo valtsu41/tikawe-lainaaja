005bc0354d22a30d54acf9e57df9fae9fb2da0dd
valtsu41 <42544217+valtsu41@users.noreply.github.com>
2026-03-29 21:33:22 +0300

    Basic listing of posts and adding posts

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import db
 
 app = Flask(__name__)
@@ -10,3 +10,21 @@ def index():
     db.execute("INSERT INTO Visits (visited_at) VALUES (datetime('now'))")
     visit_count = db.query("SELECT COUNT(*) AS c FROM Visits")[0]["c"]
     return render_template("index.html", visit_count=visit_count)
+
+@app.route("/new")
+def new_post():
+    return render_template("new.html")
+
+@app.route("/posts")
+def posts():
+    posts = db.query("SELECT * FROM Posts")
+    count = len(posts)
+    return render_template("posts.html", count=count, posts=posts)
+
+@app.route("/posts", methods=("POST",))
+def add_post():
+    username = request.form["username"]
+    item = request.form["item"]
+    db.execute("INSERT INTO Posts (author, item) VALUES (?, ?)", (username, item))
+    return redirect("/posts")
+
