7fbf1dc6ff16801cea4a6f4972ff6944f2e2a749
valtsu41 <42544217+valtsu41@users.noreply.github.com>
2026-04-19 17:47:49 +0300

    Separate SQL calls to a separate module

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import sqlite3
 
 from flask import Flask, session, request, render_template, redirect, abort
-from werkzeug.security import generate_password_hash, check_password_hash
 
 import db
 import config
@@ -12,8 +11,8 @@ app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    db.execute("INSERT INTO Visits (visited_at) VALUES (datetime('now'))")
-    visit_count = db.query("SELECT COUNT(*) AS c FROM Visits")[0]["c"]
+    data.add_visit()
+    visit_count = data.get_visit_count()
     return render_template("index.html", visit_count=visit_count)
 
 @app.route("/register")
@@ -27,10 +26,8 @@ def do_register():
     passwd2 = request.form["password2"]
     if passwd1 != passwd2:
         return "Salasanat eivät täsmää"
-    passwd_hash = generate_password_hash(passwd1)
-    try:
-        db.execute("INSERT INTO Users (username, password_hash) VALUES (?, ?)", [name, passwd_hash])
-    except sqlite3.IntegrityError:
+    user_id = data.create_user(name, passwd1)
+    if user_id is None:
         return "VIRHE: tunnus on jo varattu"
 
     return "Tunnus luotu"
@@ -43,11 +40,7 @@ def login():
 def do_login():
     name = request.form["username"]
     passwd = request.form["password"]
-
-    res = db.query("SELECT id, password_hash FROM Users WHERE username = ?", [name])
-    if res:
-        user_id, passwd_hash = res[0]
-        if check_password_hash(passwd_hash, passwd):
+    if (user_id := data.check_login(name, passwd)):
             session["user_id"] = user_id
             session["username"] = name
             return redirect("/")
@@ -69,12 +62,7 @@ def new_post():
 @app.route("/posts")
 def posts():
     query = request.args.get("query", "")
-    sql_query = f"%{query}%"
-    posts = db.query("""
-        SELECT P.id AS id, U.username AS author_name, P.item AS item
-        FROM Posts AS P JOIN Users AS U ON P.author = U.id
-        WHERE P.item LIKE ?
-    """, [sql_query])
+    posts = data.search_posts(query)
     count = len(posts)
     return render_template("posts.html", query=query, count=count, posts=posts)
 
@@ -82,18 +70,13 @@ def posts():
 def add_post():
     item = request.form["item"]
     info = request.form["info"]
-    post_id = db.execute("INSERT INTO Posts (author, item, info) VALUES (?, ?, ?)", [session["user_id"], item, info])
+    post_id = data.create_post(session["user_id"], item, info)
     return redirect(f"/posts/{post_id}")
 
 @app.route("/posts/<int:post_id>")
 def get_post(post_id):
-    res = db.query("""
-        SELECT U.username As author_name, P.item AS item, P.info AS info
-        FROM Posts AS P JOIN Users AS U ON P.author = U.id
-        WHERE P.id = ?
-    """, [post_id])
-    if not res:
+    if not (post := data.get_post(post_id)):
         abort(404)
-    db.execute("INSERT INTO Views (viewed_at, user, post) VALUES (datetime('now'), ?, ?)", [session["user_id"], post_id])
-    count = db.query("SELECT COUNT(*) FROM (SELECT DISTINCT user, post FROM Views)")[0][0]
-    return render_template("post.html", post=res[0], view_count=count)
+    data.add_post_view(session["user_id"], post_id)
+    count = data.get_post_viewer_count(post_id)
+    return render_template("post.html", post=post, view_count=count)
---
+++ data.py
@@ -0,0 +1,64 @@
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import db
+
+
+# Visitor counter functions
+
+def get_visit_count():
+    return db.query("SELECT COUNT(*) FROM Visits")[0][0]
+
+def add_visit():
+    db.execute("INSERT INTO Visits (visited_at) VALUES (datetime('now'))")
+
+
+# User functions
+
+def check_login(username: str, passwd: str):
+    res = db.query("SELECT id, password_hash FROM Users WHERE username = ?", [username])
+    if res:
+        user_id, passwd_hash = res[0]
+        if check_password_hash(passwd_hash, passwd):
+            return user_id
+    return None
+
+def create_user(username: str, passwd: str):
+    passwd_hash = generate_password_hash(passwd)
+    try:
+        return db.execute("INSERT INTO Users (username, password_hash) VALUES (?, ?)", [username, passwd_hash])
+    except sqlite3.IntegrityError:
+        return None
+
+# Post functions 
+
+def get_post(post_id: int):
+    res = db.query("""
+        SELECT U.username As author_name, P.item AS item, P.info AS info
+        FROM Posts AS P JOIN Users AS U ON P.author = U.id
+        WHERE P.id = ?
+    """, [post_id])
+    if not res:
+        return None
+    else:
+        return res[0]
+
+def create_post(author_id: int, item: str, info: str):
+    return db.execute("INSERT INTO Posts (author, item, info) VALUES (?, ?, ?)", [author_id, item, info])
+
+def search_posts(query: str):
+    sql_query = f"%{query}%"
+    return db.query("""
+        SELECT P.id AS id, U.username AS author_name, P.item AS item
+        FROM Posts AS P JOIN Users AS U ON P.author = U.id
+        WHERE P.item LIKE ?
+    """, [sql_query])
+
+
+# Post viewer functions
+
+def add_post_view(user_id: int, post_id: int):
+    return db.execute("INSERT INTO Views (viewed_at, user, post) VALUES (datetime('now'), ?, ?)", [user_id, post_id])
+
+def get_post_viewer_count(post_id: int):
+    return db.query("SELECT COUNT(*) FROM (SELECT DISTINCT user, post FROM Views WHERE post = ?)", [post_id])[0][0]
